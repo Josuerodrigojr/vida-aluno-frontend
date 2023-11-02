ea354dc12523fcc84a2167a556580441199882d1
Josuerodrigojr <98060884+Josuerodrigojr@users.noreply.github.com>
2023-11-02 19:41:40 -0300

    Começando o frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,60 @@
+import React, {useEffect, useState} from 'react'
+
+import './app.css'
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText
+} from 'mdb-react-ui-kit';
 
 function App() {
+
+  const [repositorio, setRepositorio] = useState([])
+
+  useEffect(()=>{
+    async function carregaRepositorios(){
+      const resposta = await fetch(
+        "https://vida-de-aluno.onrender.com/alunos"
+      )
+      const repositorios = await resposta.json()
+      setRepositorio(repositorios)
+      console.log(repositorios)
+    }
+    carregaRepositorios();
+  }, [])
+
+  
+
+  
   return (
-    <h1>Teste</h1>
+<>
+
+
+
+      {repositorio.map((repositorio)=>(
+       <div className="App" key={repositorio.id}>
+       <MDBCard>
+      
+      <MDBCardBody>
+        <MDBCardTitle>Aluno: {repositorio.primeironome}  {repositorio.segundonome}</MDBCardTitle>
+        <MDBCardText>
+          Turma: {repositorio.turma}
+        </MDBCardText>
+
+      </MDBCardBody>
+    </MDBCard>
+     </div>
+      ))}
+
+    
+
+    
+
+        
+        
+
+    </>
   );
 }
 
